5187adadb4fa6ea63d1c69e5ccae0611ae6b247e
isidro <idperez@asu.edu>
2016-09-10 13:43:53 -0700

    main activity on clicks set

--- app/src/main/java/com/example/isidro/toka/MainActivity.java
+++ app/src/main/java/com/example/isidro/toka/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.isidro.toka;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.IdRes;
 import android.os.Bundle;
 
 import android.util.Log;
+import android.view.View;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends Activity {
+
+    public View newsletter;
+    public View tee_times;
+    public View scorecard;
+    public View weather;
+
+    public View facebook;
+    public View instagram;
+    public View twitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,49 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         setBottomBar();
+        setViews();
+    }
+
+    private void setViews() {
+        newsletter = findViewById(R.id.newsletter);
+        setOnclick(newsletter, getResources().getString(R.string.newsletter_link));
+
+        tee_times = findViewById(R.id.tee_times);
+        setOnclick(tee_times, getResources().getString(R.string.tee_times_link));
+
+        scorecard = findViewById(R.id.scorecard);
+        setOnclick(scorecard, getResources().getString(R.string.scorecard_link));
+
+        weather = findViewById(R.id.weather);
+        setOnclick(weather, getResources().getString(R.string.weather_link));
+
+        facebook = findViewById(R.id.facebook);
+        setOnclick(facebook, getResources().getString(R.string.facebook));
+
+        instagram = findViewById(R.id.instagram);
+        setOnclick(instagram, getResources().getString(R.string.instagram));
+
+        twitter = findViewById(R.id.twitter);
+        setOnclick(twitter, getResources().getString(R.string.twitter));
+    }
+
+    private void setOnclick(View view, final String link) {
+
+        view.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(setClickableLink(link));
+            }
+        });
+    }
+
+    private Intent setClickableLink(String link) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse(link));
+        return  intent;
     }
 
     private void setBottomBar() {
