0b9eadaa3b9cec10f8aeefb9d01ff56df7e1645a
isidro <idperez@asu.edu>
2016-09-10 12:32:56 -0700

    cleaned up main activity

--- app/src/main/java/com/example/isidro/toka/MainActivity.java
+++ app/src/main/java/com/example/isidro/toka/MainActivity.java
@@ -1,36 +1,14 @@
 package com.example.isidro.toka;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.os.Build;
-import android.provider.Settings;
 import android.support.annotation.IdRes;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.TextView;
-import android.location.LocationManager;
 
 import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnTabReselectListener;
 import com.roughike.bottombar.OnTabSelectListener;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 public class MainActivity extends Activity {
 
@@ -41,7 +19,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
 
         final Intent golf = new Intent(this, Golf.class);
         final Intent list = new Intent(this, List.class);
